f92002c82d4825eae507c8c7cf2c298d545f2c8c
chandrafate <chandrapangestu666@gmail.com>
2020-12-03 09:26:58 +0700

    first

--- app/src/main/java/com/candra/sewakameraapp/OnBoardingOneActivity.kt
+++ app/src/main/java/com/candra/sewakameraapp/OnBoardingOneActivity.kt
@@ -3,9 +3,9 @@ package com.candra.sewakameraapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OnBoardingOneActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_on_boarding_one)
     }
 }
