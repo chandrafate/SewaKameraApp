358b466ec676b6abab79307d6f431ea7bba2f74d
chandrafate <chandrapangestu666@gmail.com>
2020-12-04 02:10:04 +0700

    kategori progress

---
+++ app/src/main/java/com/candra/sewakameraapp/MenuKategoriActivity.kt
@@ -0,0 +1,11 @@
+package com.candra.sewakameraapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuKategoriActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_kategori)
+    }
+}
--- app/src/main/java/com/candra/sewakameraapp/home/HomeFragment.kt
+++ app/src/main/java/com/candra/sewakameraapp/home/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.candra.sewakameraapp.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.candra.sewakameraapp.MenuKategoriActivity
 import com.candra.sewakameraapp.R
 import com.candra.sewakameraapp.utils.Preferences
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -30,6 +33,10 @@ class HomeFragment : Fragment() {
         preferences = Preferences(activity!!.applicationContext)
 
         tv_nama.setText(preferences.getValues("nama"))
+
+        btn_kategori.setOnClickListener {
+            startActivity(Intent(context, MenuKategoriActivity::class.java))
+        }
 
 
         loadDummyData()
